7e928e079d10b1bfd6bfb05715cec4954a7e9a2d
hardik <87580981+hrdkmishra@users.noreply.github.com>
2022-04-30 14:40:52 +0530

    Update contextMenu.bat

--- contextMenu.bat
+++ contextMenu.bat
@@ -3,5 +3,6 @@
 set word=code
 @REM paste the extact path here ↓
 set path=C:\Users\hardman\AppData\Local\Programs\Microsoft VS Code\Code.exe
-
+@REM add application icon here ↓
+REG ADD HKEY_CLASSES_ROOT\Directory\Background\shell\%word% /v Icon /t REG_SZ /d "%path%" /f
 REG ADD HKEY_CLASSES_ROOT\Directory\Background\shell\%word%\command /t REG_SZ /d "%path%" /f
